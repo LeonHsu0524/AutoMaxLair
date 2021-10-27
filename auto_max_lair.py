8a884929c1b81008f54e549f38d887cc0c1cbe1d
LeonHsu0524 <34006562+LeonHsu0524@users.noreply.github.com>
2021-10-27 23:23:28 +0800

    Updated OCR and Config Setting

--- auto_max_lair.py
+++ auto_max_lair.py
@@ -17,7 +17,7 @@ import toml
 import automaxlair
 from automaxlair import matchup_scoring
 
-VERSION = 'v1.0'
+VERSION = 'EasyOCR'
 
 # load configuration from the config file
 try:
@@ -224,6 +224,7 @@ def battle(ctrlr) -> str:
                 run.dmax_timer = 0
             ctrlr.push_buttons((b'a', 1.5), (b'b', 1 + VIDEO_EXTRA_DELAY))
         elif battle_state == 'FIGHT':
+            phrases_local_auto_max_lair = config[config['language']['LANGUAGE']]
             # Before the bot makes a decision, it needs to know what the boss
             # is.
             if run.opponent is None:
@@ -239,7 +240,10 @@ def battle(ctrlr) -> str:
                         (b'y', 1), (b'a', 1), (b'l', 3 + VIDEO_EXTRA_DELAY))
                     run.opponent = ctrlr.read_selectable_pokemon('battle')[0]
                     ctrlr.push_buttons((b'b', 1), (b'b', 1.5), (b'b', 2))
-
+                    if phrases_local_auto_max_lair['EASYOCR_LANG_NAME'] == 'ch_tra':
+                        if run.opponent.name_id == '百變怪':
+                            ctrlr.log("we found a Ditto in chinese")
+                            run.opponent.name_id = ditto
                     if run.opponent.name_id == 'ditto':
                         if run.current_node.name != 'normal':
                             ctrlr.log(
@@ -254,6 +258,13 @@ def battle(ctrlr) -> str:
 
                 # If our Pokemon is Ditto, transform it into the boss (or vice
                 # versa).
+                if phrases_local_auto_max_lair['EASYOCR_LANG_NAME'] == 'ch_tra':
+                    if run.opponent.name_id == '百變怪':
+                        ctrlr.log("we found a Ditto in chinese")
+                        run.opponent.name_id = 'ditto'
+                    if run.pokemon.name_id == '百變怪':
+                        ctrlr.log("we found a Ditto in chinese")
+                        run.pokemon.name_id = 'ditto'
                 if run.pokemon.name_id == 'ditto':
                     run.pokemon = matchup_scoring.transform_ditto(
                         run.pokemon, run.opponent)
@@ -416,6 +427,11 @@ def catch(ctrlr) -> str:
         )
         ctrlr.log(f'HP of current team: {[x.HP for x in team]}.', 'DEBUG')
         for potential_team in potential_teams:
+            #Changed ditto name from deteced language to english
+            if potential_team[0].name_id == '百變怪':
+                potential_team[0].name_id = 'ditto'
+            if run.boss_pokemon[run.boss].name_id == '百變怪':
+                run.potential_boss_pokemon.values().name_id = 'ditto'
             score = matchup_scoring.get_weighted_score(
                 matchup_scoring.evaluate_average_matchup(
                     potential_team[0], run.potential_boss_pokemon.values(),
@@ -616,26 +632,6 @@ def select_pokemon(ctrlr) -> str:
     take_pokemon = False  # Set to True if a non-legendary shiny is found.
     reset_game = False  # Set to True in some cases in non-default modes.
 
-    if run.num_caught == 4 and (
-        ctrlr.check_attack_stat or ctrlr.check_speed_stat
-    ):
-        ctrlr.push_button(b'>', 1)
-        if ctrlr.check_stats():
-            ctrlr.log('******************************')
-            ctrlr.log('****Matching stats found!*****')
-            ctrlr.log('******************************')
-            ctrlr.display_results(screenshot=True)
-            ctrlr.send_discord_message(
-                f"Matching stats found for {ctrlr.boss}!",
-                path_to_picture=f'logs/{ctrlr.log_name}_cap_'
-                f'{ctrlr.num_saved_images}.png',
-                embed_fields=ctrlr.get_stats_for_discord(),
-                level="legendary"
-            )
-            return None  # End whenever a matching stats legendary is found
-
-        ctrlr.push_button(b'<', 1)
-
     for i in range(run.num_caught):
         # First check if we need to reset immediately.
         # Note that "keep path" mode resets always unless a shiny legendary.
@@ -671,19 +667,45 @@ def select_pokemon(ctrlr) -> str:
             )
             ctrlr.shinies_found += 1
             ctrlr.log("Sending off to save a screenshot", "DEBUG")
-            ctrlr.display_results(screenshot=True)
-            ctrlr.push_buttons((b'p', 1), (b'b', 3), (b'p', 1))
             if run.num_caught == 4 and i == 0:
                 # NOTE: this is when we found a legendary!
-                ctrlr.send_discord_message(
-                    f'Found a shiny {run.caught_pokemon[3]}!',
-                    path_to_picture=f'logs/{ctrlr.log_name}_cap_'
-                    f'{ctrlr.num_saved_images}.png',
-                    embed_fields=ctrlr.get_stats_for_discord(),
-                    level="legendary"
-                )
-                return None  # End whenever a shiny legendary is found.
+                if ctrlr.check_attack_stat or ctrlr.check_speed_stat:
+                    ctrlr.push_button(b'>', 1)
+                    ctrlr.log('check stat start')
+                    if ctrlr.check_stats():
+                        ctrlr.log('*******************************')
+                        ctrlr.log('**Matching shiny stats found!**')
+                        ctrlr.log('*******************************')
+                        ctrlr.display_results(screenshot=True)
+                        ctrlr.push_buttons((b'p', 1), (b'b', 3), (b'p', 1))
+                        ctrlr.send_discord_message(
+                            f"Matching stats found for shiny {ctrlr.boss}!",
+                            path_to_picture=f'logs/{ctrlr.log_name}_cap_'
+                            f'{ctrlr.num_saved_images}.png',
+                            embed_fields=ctrlr.get_stats_for_discord(),
+                            level="legendary"
+                        )
+                        return None  # End whenever a matching stats legendary is found
+                    ctrlr.push_buttons((b'<', 1),(b'^', 3 + VIDEO_EXTRA_DELAY))
+                    ctrlr.send_discord_message(
+                            f"Found Non-Matching Stat for shiny {ctrlr.boss} will countinun",
+                            embed_fields=ctrlr.get_stats_for_discord(),
+                            level="legendary"
+                    )
+                if not ctrlr.check_attack_stat and not ctrlr.check_speed_stat:
+                    ctrlr.display_results(screenshot=True)
+                    ctrlr.push_buttons((b'p', 1), (b'b', 3), (b'p', 1))
+                    ctrlr.send_discord_message(
+                        f'Found a shiny {run.caught_pokemon[3]}!',
+                        path_to_picture=f'logs/{ctrlr.log_name}_cap_'
+                        f'{ctrlr.num_saved_images}.png',
+                        embed_fields=ctrlr.get_stats_for_discord(),
+                        level="legendary"
+                    )
+                    return None  # End whenever a shiny legendary is found.
             else:
+                ctrlr.display_results(screenshot=True)
+                ctrlr.push_buttons((b'p', 1), (b'b', 3), (b'p', 1))
                 ctrlr.send_discord_message(
                     f'Found a shiny '
                     f'{run.caught_pokemon[run.num_caught - 1 - i]}!',
